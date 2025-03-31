b9ff68ad6f463504601b648863d9f74a277e7413
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2025-03-31 18:16:32 -0300

    Atualização do operador lógico AND

--- aula21.py
+++ aula21.py
@@ -4,20 +4,23 @@
 # verdadeiras.
 # Se qualquer valor for considerado falso,
 # a expressão inteira será avaliada naquele valor
-# São considerados falsy (que vc já viu)
+# São considerados false (que vc já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
-# entrada = input('[E]ntrar [S]air: ')
-# senha_digitada = input('Senha: ')
 
-# senha_permitida = '123456'
+#entrada = input('[E]ntrda [S]aida: ')
+#senha_digitada = input('Senha: ')
 
-# if entrada == 'E' and senha_digitada == senha_permitida:
-#     print('Entrar')
-# else:
-#     print('Sair')
+#senha_de_entrada = '123456'
 
-# Avaliação de curto circuito
-print(True and False and True)
+#if entrada == 'E' and senha_digitada == senha_de_entrada:
+#    print('Entrou')
+#else:
+#    print('Saiu')
+#print('Fim do programa')
+
 print(True and 0 and True)
+print(bool(0.0))
+
+
