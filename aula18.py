a86920dee7e7c7c2a9e69c3bcbb1c86869152d69
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2025-03-31 17:43:55 -0300

    if / elif / else

--- aula18.py
+++ aula18.py
@@ -1,22 +1,24 @@
-"""
-Operadores de comparação (relacionais)
-OP      Significado         Exemplo (True)
->       maior               2 > 1
->=      maior ou igual      2 >= 2
-<       menor               1 < 2
-<=      menor ou igual      2 <= 2
-==      igual               'a' == 'a'
-!=      diferente           'a' != 'b'
-"""
-maior = 2 > 1
-maior_ou_igual = 2 >= 2
-menor = 1 < 2
-menor_ou_igual = 2 <= 2
-igual = 'a' == 'a'
-diferente = 'a' != 'b'
-print(f'Olha meu print aqui\n'
-      f'{maior}\n'
-      f'{maior_ou_igual}\n'
-      f'{igual}\n'
-      f'{diferente}'
-      )
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
